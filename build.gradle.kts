f0917a756652115ae39c3264833499593e3823cf
Federico Locatelli <federicolocatelli@Mac.station>
2026-06-04 17:19:08 +0200

    chore: entry point e configurazione build Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     java
     application
+    id("com.diffplug.spotless") version "6.25.0"
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        removeUnusedImports()
+    }
 }
 
 repositories {
@@ -9,8 +17,9 @@ repositories {
 
 dependencies {
     implementation("com.mysql:mysql-connector-j:8.3.0")
-    
+
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 application {
---
+++ src/main/java/it/unibo/workout/Main.java
@@ -0,0 +1,9 @@
+package it.unibo.workout;
+
+import it.unibo.workout.view.LoginView;
+
+public class Main {
+  public static void main(String[] args) {
+    new LoginView().avvia();
+  }
+}
